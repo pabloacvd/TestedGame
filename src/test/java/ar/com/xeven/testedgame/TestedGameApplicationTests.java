65ac3bcf63a2913a1aa6f32a04f5169a44720616
pabloacvd <pablo@xeven.com.ar>
2021-07-31 13:02:30 -0300

    appending new tests!

---
+++ src/test/java/ar/com/xeven/testedgame/TestedGameApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.xeven.testedgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestedGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
